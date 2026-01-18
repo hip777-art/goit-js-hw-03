797900f21d7bf8e07a48159850e2adefcbaf9981
hip777-art <arkhip24032000@gmail.com>
2026-01-18 18:31:12 +0200

    Implement filterArray function to filter numbers

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+function filterArray(numbers, value) {
+  const arrayResult = [];
+
+  for (const elem of numbers) {
+    if (elem > value) {
+      arrayResult.push(elem);
+    }
+  }
+
+  return arrayResult;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
